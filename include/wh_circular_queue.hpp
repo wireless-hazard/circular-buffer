e73876fc7dd51ebe31a1424572d419b5e1d24f92
Magno <magnocostamaia@gmail.com>
2022-09-15 09:53:06 -0300

    Added option to choose if buffer needs to be allocated by the class

--- include/wh_circular_queue.hpp
+++ include/wh_circular_queue.hpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include "esp_err.h"
+#include <cstdlib>
 
 namespace wh_queue
 {
@@ -9,7 +10,15 @@ namespace wh_queue
 template <typename T, size_t L>
 class CircularQueue{
 public:
-	inline CircularQueue(void): front(-1), back(-1){}
+	inline explicit CircularQueue(void): own_buffer(true), front(-1), back(-1){
+		internal_buffer = (T *)malloc(L*sizeof(T));
+	}
+	inline explicit CircularQueue(T* pre_alloc): own_buffer(false){
+		internal_buffer = pre_alloc;
+	}
+	inline ~CircularQueue(void){
+		if (own_buffer){free(internal_buffer);}
+	}
 	/**
 	@brief Returns the oldest value from the queue, removing it afterwords
 	return T Returns the value from queue
@@ -36,12 +45,15 @@ public:
 		internal_buffer[back] = value;
 		return ESP_OK;
 	}
+private:
+	bool own_buffer;
+
 protected:
 	/**
 	@brief Returns true if the queue is empty
 	*/
 	inline bool isEmpty(void) const {return ((front==(-1)) && (back == (-1)));}
-	T internal_buffer[L];
+	T *internal_buffer;
 	int front;
 	int back;
 };
